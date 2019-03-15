5f5417584df440437f4c71fc11482b59e1dd2495
asolanky <alwinsolanky@gmail.com>
2019-03-15 06:19:12 +0000

    day 9 part 2

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ new Vue({
    km: 0,
    m: 0
   },
-  watch: {
-    km() {
-      // whenever km changes, this function runs
+ watch: {
+   km() {
+    // whenever km changes, this function runs
     },
+    m() {
+      // whenever m changes - this function runs
+    }
   }
 });
