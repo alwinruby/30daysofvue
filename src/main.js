6df6f72502337dcf11a0dbb72d2eef9ea917053e
asolanky <alwinsolanky@gmail.com>
2019-03-16 10:27:06 +0000

    day 9 part 4

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 new Vue({
-  el: '#app',
+ el: '#app',
   data: {
-     km: 0,
-     m: 0
-   },
-   watch: {
-    km() {
-      this.m = this.km ? this.km * 1000 : 0;
-    },
-    m() {
-      this.km = this.m ? this.m / 1000 : 0;
+    km: 0
+  },
+  computed: {
+    m: {
+      get() {
+        return this.km * 1000;
+      },
+      set(newValue) {
+        this.km = newValue/1000;
+      }
     }
   }
 });
